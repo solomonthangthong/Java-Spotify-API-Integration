7d82e68fd5be2fc53c7a3be30975c21de53f94f2
Solomon Thangthong <than0047@algonquinlive.com>
2023-06-20 15:46:00 -0400

    fix: corrected .java file, main class declaration, pom.xml update

---
+++ src/main/java/spotify/SpotifyApi.java
@@ -0,0 +1,11 @@
+package spotify;
+import se.michaelthelin.spotify.*;
+import java.io.IOException;
+
+public class SpotifyApi {
+    public static void main(String[] args){
+        String clientID;
+        String clientSecret;
+        String redirectUri;
+    }
+}
